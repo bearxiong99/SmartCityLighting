2293f817baa393d7504c6ba31dcd61c6c802b446
Kassidy Knight <Kassiknight@gmail.com>
2017-03-06 15:19:46 -0600

    re-format lamp objects, including weather/power usage/maintenance requests

--- Web App Frontend/js/app.js
+++ Web App Frontend/js/app.js
@@ -1,6 +1,6 @@
 
 var app = angular.module('LightsApp', []).controller('MainController', function($scope) {
-    $scope.modules = [
+    $scope.lamps = [
     {
         _id : '5828e870c66c89f5fbdfeeaa',
         title : 'LAMP01',
@@ -8,10 +8,54 @@ var app = angular.module('LightsApp', []).controller('MainController', function(
         maintainedBy : 'Ankit Sharma',
         address : '2809 Woodway Drive',
         lampStatus : 'Operational',
-        temperature : 72,
-        humidity : 98,
+        lampStatusImage: '../images/blank.png',
+        requests: 
+        [
+            {
+                description: 'Bulb outage',
+                dateTime: '2016-10-09T02:15:00',
+                completed: 'NO',
+                status: 'danger'
+            },
+            {
+                description: 'Faulty sensor',
+                dateTime: '2016-10-04T17:32:00',
+                completed: 'YES',
+                status: 'success'
+            },
+            {
+                description: 'Faulty sensor',
+                dateTime: '2016-08-15T20:40:00',
+                completed: 'YES',
+                status: 'success'
+            },
+            {
+                description: 'Faulty sensor',
+                dateTime: '2016-06-23T05:18:00',
+                completed: 'YES',
+                status: 'success'
+            },
+            {
+                description: 'Faulty sensor',
+                dateTime: '2016-05-08T12:24:00',
+                completed: 'YES',
+                status: 'success'
+            }
+        ],
+        weather: 
+        {
+            temperature : 72,
+            humidity : 98,
+            windSpeed: 12
+        },
+        power_usage: 
+        {
+            today: 2,
+            monthly: 60,
+            yearly: 530
+        },
         proximity : 1,
-        collapse: 'collapseOne'
+        collapse_id: 'collapseOne'
     },
     {
         _id : '5828e870c66c89f5fbdfeeab',
@@ -20,11 +64,21 @@ var app = angular.module('LightsApp', []).controller('MainController', function(
         maintainedBy : 'Chris Chen',
         address : '2210 Pearl Street',
         lampStatus : 'Operational',
-        temperature : 73,
-        humidity : 96,
+        lampStatusImage: '../images/blank.png',
+        weather: 
+        {
+            temperature : 73,
+            humidity : 96,
+            windSpeed: 20
+        },
+        power_usage: 
+        {
+            today: 1,
+            monthly: 52,
+            yearly: 440
+        },
         proximity : 0,
-        windSpeed : 20,
-        collapse: 'collapseTwo'
+        collapse_id: 'collapseTwo'
     },
     {
         _id : '5828e870c66c89f5fbdfeeac',
@@ -33,10 +87,21 @@ var app = angular.module('LightsApp', []).controller('MainController', function(
         maintainedBy : 'Kassidy Knight',
         address : '2222 Pearl Street',
         lampStatus : 'Needs maintenance',
-        temperature : 74,
-        humidity : 92,
+        lampStatusImage: '../images/warning.png',
+        weather: 
+        {
+            temperature : 74,
+            humidity : 92,
+            windSpeed: 5
+        },
+        power_usage: 
+        {
+            today: 4,
+            monthly: 65,
+            yearly: 500
+        },
         proximity : 0,
-        collapse: 'collapseThree'
+        collapse_id: 'collapseThree'
     },
     {
         _id : '5828e870c66c89f5fbdfeead',
@@ -45,10 +110,21 @@ var app = angular.module('LightsApp', []).controller('MainController', function(
         maintainedBy : 'Pascale Queralt',
         address : '4819 Forest Lane',
         lampStatus : 'Operational',
-        temperature : 100,
-        humidity : 99,
+        lampStatusImage: '../images/blank.png',
+        weather: 
+        {
+            temperature : 100,
+            humidity : 99,
+            windSpeed: 10
+        },
+        power_usage: 
+        {
+            today: 3,
+            monthly: 70,
+            yearly: 520
+        },
         proximity : 11,
-        collapse: 'collapseFour'
+        collapse_id: 'collapseFour'
     }
     ];
 
