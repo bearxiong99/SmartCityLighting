0b012dd7226ed600d1970acb7cbc7d63aa754f7f
Robbie Zuazua <rzuazua@utexas.edu>
2017-03-07 19:02:49 -0600

    new stuff

---
+++ Firmware/New_Stuff/sensors.h
@@ -0,0 +1,33 @@
+/*
+ * sensors.h
+ *
+ *  Created on: Mar 5, 2017
+ *      Author: curew
+ */
+
+#ifndef SENSORS_H_
+#define SENSORS_H_
+
+#include <ti/drivers/GPIO.h>
+#include <Board.h>
+#include <ti/drivers/I2C.h>
+#include <stdint.h>
+#include <ti/drivers/PIN.h>
+//#include <unistd.h>
+#include "LED.h"
+#include "opt3001.h"
+#include "packets.h"
+#include <stdlib.h>
+#include "acs712.h"
+#include "HDC1010.h"
+
+uint8_t* structToArray(SensorDataStruct* s);
+SensorDataStruct* packageData(int temp, int humidity, int light, int amps, int motions);
+uint8_t* getMeasurements(void);
+
+
+
+
+
+
+#endif /* SENSORS_H_ */
