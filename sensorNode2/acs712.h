b7c23e93466a5805d4cf449ed51b575c374f35f8
Robbie Zuazua <Rzuazua@utexas.edu>
2017-04-10 15:07:33 -0500

    Add files via upload

---
+++ sensorNode2/acs712.h
@@ -0,0 +1,24 @@
+/*
+ * acs712.h
+ *
+ *  Created on: Feb 27, 2017
+ *      Author: curew
+ */
+
+#ifndef ACS712_H_
+#define ACS712_H_
+
+#include <Board.h>
+#include <ti/drivers/I2C.h>
+#include <stdint.h>
+#include "LED.h"
+/*#include <unistd.h>*/
+
+
+/*Prepare all Hardware thread will use*/
+ADC_Handle a_init(void);
+
+/*Handle GPIO Signal*/
+uint16_t Check_Light(ADC_Handle adc);
+
+#endif /* ACS712_H_ */
