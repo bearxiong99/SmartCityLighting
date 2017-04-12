9185d302e024ef198b10bda0501d551788dcfc5c
Justin Curewitz <Justin Curewitz>
2017-04-11 19:34:15 -0500

    added motion detection

--- Firmware/src/Sensor_src/sensors.c
+++ Firmware/src/Sensor_src/sensors.c
@@ -4,19 +4,40 @@
  *  Created on: Feb 15, 2017
  *      Author: Justin Curewitz
  */
+#include "Sensors.h"
+
+
+static PIN_State motionPinState;
+static PIN_Handle motionPinHandle;
 
 /*Thread that runs the OPT3001 light sensor.*/
-
-#include "Sensors.h"
+PIN_Config motionPinTable[] = {
+    CC1310_LAUNCHXL_DIO21  | PIN_INPUT_EN | PIN_PULLUP | PIN_IRQ_NEGEDGE,
+    PIN_TERMINATE
+};
     uint8_t         txBuffer[4];
     uint8_t         rxBuffer[4];
     I2C_Handle      i2c;
     I2C_Params      i2cParams;
     I2C_Transaction i2cTransaction;
     ADC_Handle adc;
+    int motions;
+
+void motionCallbackFxn(PIN_Handle handle, PIN_Id pinId) {
+    motions += 1;
+}
 /*get interrupt from motion sensor*/
 void initMotion(void){
-
+    motions = 0;
+    motionPinHandle = PIN_open(&motionPinState, motionPinTable);
+    if(!motionPinHandle) {
+        //System_abort("Error initializing button pins\n");
+        while(1){}
+    }
+    if (PIN_registerIntCb(motionPinHandle, &motionCallbackFxn) != 0) {
+        //System_abort("Error registering button callback function");
+        while(1);
+    }
 }
 
 /*converts Sensor Struct to byte array*/
@@ -57,7 +78,7 @@ void init_sensors(void){
     i2cParams.bitRate = I2C_400kHz;
     i2c = I2C_open(Board_I2C0, &i2cParams);
     adc = a_init();
-    //int id = get_TI_ID(txBuffer, rxBuffer, i2cTransaction, i2c);
+    initMotion();
     toggleLED(0);
     toggleLED2(0);
 }
@@ -65,24 +86,20 @@ void init_sensors(void){
 uint8_t* getMeasurements(void) {
     int z = 1;
     int light = 0;
-    uint16_t config_val = 0;
     int id = get_TI_ID(txBuffer, rxBuffer, i2cTransaction, i2c);
     int good_config = config(txBuffer, rxBuffer, i2cTransaction, i2c);
     setConfig(i2c, i2cTransaction, txBuffer, rxBuffer, DEFAULT_SLAVE);
     requestTemp(i2c, i2cTransaction, txBuffer, rxBuffer, DEFAULT_SLAVE);
     //read light on loop
     while(1) {
-        //config_val = get_config(txBuffer, rxBuffer, i2cTransaction, i2c);
-        //while((get_config(txBuffer, rxBuffer, i2cTransaction, i2c) & 0x0080) == 0) {}
-        //light = get_Light(txBuffer, rxBuffer, i2cTransaction, i2c);
-
         uint16_t adc_val = Check_Light(adc);
         toggleLED2(z);
         z = !z;
         light = get_Light(txBuffer, rxBuffer, i2cTransaction, i2c);
         int temp = getTemp(i2c, i2cTransaction, txBuffer, rxBuffer, DEFAULT_SLAVE);
         int hum = (rxBuffer[2] << 8) + rxBuffer[3];
-        SensorDataStruct* sp = packageData(temp, hum, light, adc_val, 0);
+        SensorDataStruct* sp = packageData(temp, hum, light, adc_val, motions);
+        motions = 0;
 
         if (sp->temp != 0 && sp->light != 0){
             uint8_t* toSend = structToArray(sp);
